29939e0dffeb63589156e844d1b2580936bc59cd
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-08-22 23:06:11 +0530

    ✅ Reverse Nodes in k-Group | Hard | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-08-22

---
+++ hard/0025-reverse-nodes-in-k-group/solution.cpp
@@ -0,0 +1,16 @@
+
+        // Check whether k nodes are available
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;
+public:
+class Solution {
+ */
+ * };
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode *next;
+ * struct ListNode {
+ *     int val;
+ * Definition for singly-linked list.
+/**
