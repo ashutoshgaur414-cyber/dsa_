4eff59cbb170579a0642c1c99e25cde27f47dee3
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-08-24 21:56:23 +0530

    ✅ Stone Game VIII | Hard | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-08-24

---
+++ hard/1872-stone-game-viii/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        vector<long long> P(n);
+        P[0] = stones[0];
+        for (int i = 1; i < n; i++)
+            P[i] = P[i - 1] + stones[i];
+
+        long long dp = P[n - 1]; // best score diff if it's your turn starting at index n-1
+        for (int i = n - 2; i >= 1; i--) {
+            dp = max(dp, P[i] - dp);
+        }
+        return (int)dp;
+    }
+};
