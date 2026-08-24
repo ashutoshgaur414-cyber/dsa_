032a332d3a986b991b308b6cf62a6863f873fa5d
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-08-24 22:25:40 +0530

    ✅ Remove Element | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 11.6 MB | 2026-08-24

--- easy/0027-remove-element/solution.cpp
+++ easy/0027-remove-element/solution.cpp
@@ -1,15 +1,15 @@
-class Solution {
+class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int i = 0;
-        int j = nums.size();
-        for(int j = 0;j<nums.size();j++)
-        {
-         if (nums[j] != val) {
-                nums[i] = nums[j];
-                i++;
-            }
-        }
-        return i;
-    }
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        int j = nums.size();
+        for(int j = 0;j<nums.size();j++)
+        {
+         if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
 };
