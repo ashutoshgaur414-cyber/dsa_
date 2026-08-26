81b2a20edfcfb617dcfefc8b417f42c7f83ffe98
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-08-26 22:23:30 +0530

    ✅ Two Sum | Easy | C++ | Runtime: 3 ms (66.91%) | Memory: 15 MB | 2026-08-26

--- easy/0001-two-sum/solution.cpp
+++ easy/0001-two-sum/solution.cpp
@@ -1,16 +1,18 @@
-    vector<int> twoSum(vector<int>& nums, int target) {
-      map<int,int>mpp;
-      int a,more;
-      for(int i =0;i<nums.size();i++)
-      {
-        a = nums[i];
-      } 
-        more = target-a;
-        if(mpp.find(more)!=mpp.end())
-        {
-            return {mpp[more],i};
-        }
-    }
-        mpp[a]=i;
-      return{-1,-1};
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      map<int,int>mpp;
+      int a,more;
+      for(int i =0;i<nums.size();i++)
+      {
+        a = nums[i];
+        more = target-a;
+        if(mpp.find(more)!=mpp.end())
+        {
+            return {mpp[more],i};
+        }
+        mpp[a]=i;
+      } 
+      return{-1,-1};
+    }
 };
