5a6507f36a5dc9c126544f34732d551bcb4b063f
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-08-27 20:22:59 +0530

    ✅ Lexicographically Smallest Permutation Greater Than Target | Medium | C++ | Runtime: 0 ms | Memory: 7.8 MB | 2026-08-27

---
+++ medium/3720-lexicographically-smallest-permutation-greater-than-target/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string lexGreaterPermutation(string s, string target) {
+
+        sort(s.begin(), s.end());
+
+        do {
+            if (s > target) {
+                return s;
+            }
+        } while (next_permutation(s.begin(), s.end()));
+
+        return "";
+    }
+};
