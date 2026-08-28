eed9cc147048cf689c808aa5d9aa6cf8f46074b4
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-08-28 13:25:23 +0530

    ✅ Lexicographically Smallest Palindromic Permutation Greater Than Target | Hard | C++ | Runtime: 3 ms (70.77%) | Memory: 16.1 MB | 2026-08-28

--- hard/3734-lexicographically-smallest-palindromic-permutation-greater-than-target/solution.cpp
+++ hard/3734-lexicographically-smallest-palindromic-permutation-greater-than-target/solution.cpp
@@ -1,104 +1,90 @@
+#include <vector>
+#include <array>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     string lexPalindromicPermutation(string s, string target) {
-
         int n = s.size();
+        int cntFull[26] = {0};
+        for (char c : s) cntFull[c - 'a']++;
 
-        // Count characters
-        vector<int> cnt(26, 0);
-
-        for (char c : s) {
-            cnt[c - 'a']++;
-        }
-
-        // Check whether palindrome is possible
-        int odd = 0;
-        char mid = 0;
-
+        int oddCount = 0, oddChar = -1;
         for (int i = 0; i < 26; i++) {
-            if (cnt[i] % 2 == 1) {
-                odd++;
-                mid = 'a' + i;
-            }
+            if (cntFull[i] % 2 == 1) { oddCount++; oddChar = i; }
         }
-
-        // More than one odd frequency -> impossible
-        if (odd > 1) {
+        // A palindromic permutation must have at most one odd-count letter,
+        // and exactly one if n is odd, none if n is even.
+        if ((n % 2 == 0 && oddCount != 0) || (n % 2 == 1 && oddCount != 1))
             return "";
-        }
 
-        // Build counts for LEFT HALF
-        vector<int> half(26, 0);
+        int h = n / 2;
+        int M[26];
+        for (int i = 0; i < 26; i++) M[i] = cntFull[i] / 2; // multiset for the first half
 
-        for (int i = 0; i < 26; i++) {
-            half[i] = cnt[i] / 2;
-        }
+        // Snapshot of available counts BEFORE placing position p (tight path).
+        vector<array<int,26>> snapshots(h);
+        array<int,26> cur;
+        for (int i = 0; i < 26; i++) cur[i] = M[i];
 
-        int m = n / 2;
-
-        // Function to make palindrome from left half
-        auto makePalindrome = [&](string left) {
-
-            string ans = left;
-
-            if (n % 2 == 1) {
-                ans += mid;
+        int failPos = -1;
+        vector<char> tightChars(h, 0);
+        for (int p = 0; p < h; p++) {
+            snapshots[p] = cur;
+            int idx = target[p] - 'a';
+            if (cur[idx] > 0) {
+                cur[idx]--;
+                tightChars[p] = target[p];
+            } else {
+                failPos = p;
+                break;
             }
+        }
+        bool fullyTight = (failPos == -1);
 
-            reverse(left.begin(), left.end());
-            ans += left;
-
-            return ans;
+        auto buildFull = [&](vector<char>& A, int midC) {
+            string res(n, ' ');
+            for (int i = 0; i < h; i++) res[i] = A[i];
+            if (n % 2 == 1) res[h] = 'a' + midC;
+            for (int i = 0; i < h; i++) res[n - 1 - i] = A[i];
+            return res;
         };
 
-        // Try constructing the left half
-        string left = "";
-
-        for (int pos = 0; pos < m; pos++) {
-
-            bool found = false;
-
-            // Try smallest possible character
-            for (int c = 0; c < 26; c++) {
-
-                if (half[c] == 0)
-                    continue;
-
-                // Choose this character
-                half[c]--;
-                left.push_back('a' + c);
-
-                // Put remaining characters in smallest order
-                string temp = left;
-
-                for (int x = 0; x < 26; x++) {
-                    temp += string(half[x], 'a' + x);
+        auto tryBump = [&](int p) -> string {
+            array<int,26>& snap = snapshots[p];
+            int tc = target[p] - 'a';
+            for (int c = tc + 1; c < 26; c++) {
+                if (snap[c] > 0) {
+                    vector<char> B(h);
+                    for (int i = 0; i < p; i++) B[i] = target[i];
+                    B[p] = 'a' + c;
+                    array<int,26> rem = snap;
+                    rem[c]--;
+                    int idx2 = p + 1;
+                    for (int cc = 0; cc < 26; cc++)
+                        while (rem[cc] > 0) { B[idx2++] = 'a' + cc; rem[cc]--; }
+                    return buildFull(B, oddChar);
                 }
-
-                // Construct palindrome
-                string candidate = makePalindrome(temp);
-
-                // If this choice can produce a palindrome > target
-                if (candidate > target) {
-                    found = true;
-                    break;
-                }
-
-                // Undo choice
-                left.pop_back();
-                half[c]++;
             }
+            return "";
+        };
 
-            if (!found) {
-                return "";
+        if (fullyTight) {
+            vector<char> A = tightChars;
+            string cand = buildFull(A, oddChar);
+            if (cand > target) return cand;
+            for (int p = h - 1; p >= 0; p--) {
+                string r = tryBump(p);
+                if (!r.empty()) return r;
             }
+            return "";
+        } else {
+            for (int p = failPos; p >= 0; p--) {
+                string r = tryBump(p);
+                if (!r.empty()) return r;
+            }
+            return "";
         }
-
-        string answer = makePalindrome(left);
-
-        if (answer > target)
-            return answer;
-
-        return "";
     }
 };
