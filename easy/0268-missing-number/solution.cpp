36d9f443a1855c79aee5e47ea684b7900a89f438
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-08-28 12:45:55 +0530

    ✅ Missing Number | Easy | C++ | Runtime: 3 ms (21.14%) | Memory: 22.5 MB | 2026-08-28

--- easy/0268-missing-number/solution.cpp
+++ easy/0268-missing-number/solution.cpp
@@ -1,8 +1,19 @@
-            }
-        }
-            return -1;
-    }
-                return i;
-            {
-            if(hash[i]==0)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+       vector<int>hash(n+1,0);
+        for(int i = 0;i<n;i++)
+        {
+            hash[nums[i]]++;
+        }
+        for(int i = 1;i<=n;i++)
+        {
+            if(hash[i]==0)
+            {
+                return i;
+            }
+        }
+            return 0;
+    }
 };
