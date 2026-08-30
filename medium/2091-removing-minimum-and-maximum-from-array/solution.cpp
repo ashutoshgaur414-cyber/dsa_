db3225f28132d834679dcf474a69cbcd1cbe4853
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-08-30 21:05:26 +0530

    ✅ Removing Minimum and Maximum From Array | Medium | C++ | Runtime: 3 ms (36.81%) | Memory: 92.2 MB | 2026-08-30

--- medium/2091-removing-minimum-and-maximum-from-array/solution.cpp
+++ medium/2091-removing-minimum-and-maximum-from-array/solution.cpp
@@ -1,9 +1,23 @@
-        int fromFront = j + 1;
-        // Option 2: remove both from back
-        int fromBack = n - i;
-        // Option 3: remove one from front (up to i), one from back (from j)
-        int mixed = (i + 1) + (n - j);
-        
-        return min({fromFront, fromBack, mixed});
-    }
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n = nums.size();
+        int minIdx = 0, maxIdx = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[minIdx]) minIdx = i;
+            if (nums[i] > nums[maxIdx]) maxIdx = i;
+        }
+        
+        int i = min(minIdx, maxIdx);
+        int j = max(minIdx, maxIdx);
+        
+        // Option 1: remove both from front
+        int fromFront = j + 1;
+        // Option 2: remove both from back
+        int fromBack = n - i;
+        // Option 3: remove one from front (up to i), one from back (from j)
+        int mixed = (i + 1) + (n - j);
+        
+        return min({fromFront, fromBack, mixed});
+    }
 };
