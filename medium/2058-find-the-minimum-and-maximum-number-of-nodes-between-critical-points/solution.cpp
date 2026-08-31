d3754fdfbf3a59a0e6d884d27a8b506385621aaa
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-08-31 20:47:51 +0530

    ✅ Find the Minimum and Maximum Number of Nodes Between Critical Points | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 124.6 MB | 2026-08-31

--- medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.cpp
+++ medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.cpp
@@ -1,28 +1,36 @@
-        while (curr->next != nullptr) {
-            bool isMaxima = curr->val > prev->val && curr->val > curr->next->val;
-            bool isMinima = curr->val < prev->val && curr->val < curr->next->val;
-            
-            if (isMaxima || isMinima) {
-                if (firstIdx == -1) {
-                    firstIdx = idx;
-                } else {
-                    minDist = min(minDist, idx - prevIdx);
-                }
-                prevIdx = idx;
-            }
-            
-            prev = curr;
-            curr = curr->next;
-            idx++;
-        }
-        
-        if (firstIdx == -1 || firstIdx == prevIdx) {
-        
-        ListNode* curr = head->next;
-        
-        ListNode* prev = head;
-        int firstIdx = -1, prevIdx = -1, idx = 1;
-        int minDist = INT_MAX;
-    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-class Solution {
+class Solution {
 public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        int firstIdx = -1, prevIdx = -1, idx = 1;
+        int minDist = INT_MAX;
+        
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+        
+        while (curr->next != nullptr) {
+            bool isMaxima = curr->val > prev->val && curr->val > curr->next->val;
+            bool isMinima = curr->val < prev->val && curr->val < curr->next->val;
+            
+            if (isMaxima || isMinima) {
+                if (firstIdx == -1) {
+                    firstIdx = idx;
+                } else {
+                    minDist = min(minDist, idx - prevIdx);
+                }
+                prevIdx = idx;
+            }
+            
+            prev = curr;
+            curr = curr->next;
+            idx++;
+        }
+        
+        if (firstIdx == -1 || firstIdx == prevIdx) {
+            // fewer than 2 critical points found
+            return {-1, -1};
+        }
+        
+        int maxDist = prevIdx - firstIdx;
+        return {minDist, maxDist};
+    }
+};
