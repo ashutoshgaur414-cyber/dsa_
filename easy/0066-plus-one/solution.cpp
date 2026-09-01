e106c7991199cc99f4588ecf480afdfac0cf9da8
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-01 19:55:17 +0530

    ✅ Plus One | Easy | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-09-01

---
+++ easy/0066-plus-one/solution.cpp
@@ -0,0 +1,20 @@
+    vector<int> plusOne(vector<int>& digits) {
+        
+
+     for(int i = digits.size()-1;i>=0;i--)
+     return digits;
+
+     {
+        if(digits[i]<9)
+     }  
+        {
+            digits[i]++;
+        }
+    }
+            return digits;
+        digits[i]=0;
+
+     digits.insert(digits.begin(),1);
+};
+public:
+class Solution {
