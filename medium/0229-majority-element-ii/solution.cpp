9ab18f662a4cf4427ac06fa94f7bcd20fc9bf3fc
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-01 20:26:38 +0530

    ✅ Majority Element II | Medium | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-09-01

---
+++ medium/0229-majority-element-ii/solution.cpp
@@ -0,0 +1,11 @@
+            for(int el: nums)
+            {
+                if(el == val)
+            int count = 0;
+        {
+        for(int val : nums)
+        vector<int>x;
+        int n = nums.size();
+    vector<int> majorityElement(vector<int>& nums) {
+public:
+class Solution {
