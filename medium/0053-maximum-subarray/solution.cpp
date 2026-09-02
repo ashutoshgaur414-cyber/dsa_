adb7d39c8a9ac924845ffa233cf90c43881f96d6
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-02 22:38:30 +0530

    ✅ Maximum Subarray | Medium | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-09-02

--- medium/0053-maximum-subarray/solution.cpp
+++ medium/0053-maximum-subarray/solution.cpp
@@ -1,18 +1,17 @@
-class Solution {
+class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int currentsum = 0;
-        int maxsum = INT_MIN;
-        for(int i:nums)
-        {
-            currentsum = currentsum + i;
-            maxsum = max(currentsum,maxsum);
-            if(currentsum<0)
-            {
-                currentsum = 0;
-            }
-        }
-        return maxsum;
-        
-    }
+    int maxSubArray(vector<int>& nums) {
+    int sum = 0;
+    for(int i =0;i<nums.size();i++)
+    {
+        sum = sum + nums[i];
+    }
+      return maxsum;  
+        maxsum = max(maxsum,sum);
+    int maxsum= INT_MIN;
+        if(sum<0)
+        {
+            sum=0;
+        }
+    }
 };
