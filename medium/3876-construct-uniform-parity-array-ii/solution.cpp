f4d53b1fea8ee1b893b3bd76bae25dd88f33093a
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-03 19:31:13 +0530

    ✅ Construct Uniform Parity Array II | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-03

--- medium/3876-construct-uniform-parity-array-ii/solution.cpp
+++ medium/3876-construct-uniform-parity-array-ii/solution.cpp
@@ -1,6 +1,17 @@
-class Solution {
-public:
-    bool uniformArray(vector<int>& nums1) {
-        return true ;
+                int oppositeMin = (parity == 0) ? minOdd : minEven;
+
+                if (oppositeMin == INT_MAX || nums1[i] <= oppositeMin)
+                    return false;
+
+                // We can use nums1[i] - oppositeMin
+                // which is positive and odd.
+                if (parity == 0)
+                    minEven = min(minEven, nums1[i]);
+                else
+                    minOdd = min(minOdd, nums1[i]);
+            }
+        }
+
+        return true;
     }
 };
