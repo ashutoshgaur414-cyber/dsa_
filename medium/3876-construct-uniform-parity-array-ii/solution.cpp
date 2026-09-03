97dda25c0f0a7c1c45e65bc06a90947312b59133
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-03 19:35:28 +0530

    ✅ Construct Uniform Parity Array II | Medium | C++ | Runtime: 4 ms (65.18%) | Memory: 165.9 MB | 2026-09-03

--- medium/3876-construct-uniform-parity-array-ii/solution.cpp
+++ medium/3876-construct-uniform-parity-array-ii/solution.cpp
@@ -1,19 +1,19 @@
-class Solution {
+class Solution {
 public:
-    bool uniformArray(vector<int>& nums1) {
+    bool uniformArray(vector<int>& nums1) {
 
-        int mn = *min_element(nums1.begin(), nums1.end());
+        int mn = *min_element(nums1.begin(), nums1.end());
 
-        // If minimum is odd, we can make everything odd
-        if (mn % 2 != 0)
-            return true;
+        // If minimum is odd, we can make everything odd
+        if (mn % 2 != 0)
+            return true;
 
-        // Minimum is even, so every element must already be even
-        for (int x : nums1) {
-            if (x % 2 != 0)
-                return false;
-        }
+        // Minimum is even, so every element must already be even
+        for (int x : nums1) {
+            if (x % 2 != 0)
+                return false;
+        }
 
-        return true;
-    }
+        return true;
+    }
 };
