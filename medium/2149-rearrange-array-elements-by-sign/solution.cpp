7e90c9d7d689d1bfd3ace8637a22f5e5a04ea8f2
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-03 22:08:14 +0530

    ✅ Rearrange Array Elements by Sign | Medium | C++ | Runtime: 3 ms (85.31%) | Memory: 128.4 MB | 2026-09-03

--- medium/2149-rearrange-array-elements-by-sign/solution.cpp
+++ medium/2149-rearrange-array-elements-by-sign/solution.cpp
@@ -1,11 +1,24 @@
-     for (int i = 0;i<nums.size();i++)
-         {
-            if(nums[i]>0)
-            {
-                ans.at(pos)=nums[i];
-        int neg = 1;
-        int pos = 0;
-    vector<int> rearrangeArray(vector<int>& nums) {
+class Solution {
 public:
-class Solution {
-        vector<int>ans(nums.size());
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int pos = 0;
+        int neg = 1;
+        vector<int>ans(nums.size());
+     for (int i = 0;i<nums.size();i++)
+         {
+            if(nums[i]>0)
+            {
+                ans.at(pos)=nums[i];
+                pos = pos+2;
+            }
+            else
+            {
+                ans.at(neg)=nums[i];
+                neg = neg +2;
+            }
+         }
+
+
+return ans;
+    }
+};
