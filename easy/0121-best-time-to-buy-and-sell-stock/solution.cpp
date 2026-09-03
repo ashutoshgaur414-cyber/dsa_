71cc76f6041808597074a72165c0449c2cdd542a
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-03 21:48:36 +0530

    ✅ Best Time to Buy and Sell Stock | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 97.3 MB | 2026-09-03

--- easy/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ easy/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,17 +1,22 @@
-      }
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      int minprice = prices[0];
+      int maxprofit = 0;
+      for(int i = 0;i<prices.size();i++)
+      {
+        maxprofit = max(maxprofit,prices[i]-minprice);
+        minprice = min(minprice,prices[i]);
 
-        minprice = min(minprice,prices[i]);
+      }
 
-      if(maxprofit<=0)
-      {
-        return 0;
-      }
-      else
-      {
-        return maxprofit;
-      }
-        maxprofit = max(maxprofit,prices[i]-minprice);
-      {
-      for(int i = 0;i<prices.size();i++)
-    }
+      if(maxprofit<=0)
+      {
+        return 0;
+      }
+      else
+      {
+        return maxprofit;
+      }
+    }
 };
