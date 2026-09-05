695ef26561488f0afc7a93c930aff42c5be9821d
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-05 18:50:36 +0530

    ✅ Flipping an Image | Easy | C++ | Runtime: 0 ms | Memory: 8.5 MB | 2026-09-05

---
+++ easy/0832-flipping-an-image/solution.cpp
@@ -0,0 +1,19 @@
+         }
+            }
+                val = val^1;
+            {
+            for(auto& val : row)
+         {
+            reverse(row.begin(),row.end());
+        
+         for(auto& row :image)
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+public:
+class Solution {
+
+
+return image ;
+
+
+    }
+};
