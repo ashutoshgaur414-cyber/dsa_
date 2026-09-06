4e158a67e9a53acc5407309b7b9b0f8687ea3868
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-06 18:19:49 +0530

    ✅ Distinct Subsequences | Hard | C++ | Runtime: 7 ms (96.20%) | Memory: 9 MB | 2026-09-06

--- hard/0115-distinct-subsequences/solution.cpp
+++ hard/0115-distinct-subsequences/solution.cpp
@@ -1,9 +1,19 @@
-        for (int i = 1; i <= m; i++) {
-
-        dp[0] = 1; // empty t can always be formed (one way: pick nothing)
-        // dp[j] = number of distinct subsequences of s[0..i) equal to t[0..j)
-        vector<unsigned long long> dp(n + 1, 0);
-    int numDistinct(string s, string t) {
-        int m = s.size(), n = t.size();
+class Solution {
 public:
-class Solution {
+    int numDistinct(string s, string t) {
+        int m = s.size(), n = t.size();
+        // dp[j] = number of distinct subsequences of s[0..i) equal to t[0..j)
+        vector<unsigned long long> dp(n + 1, 0);
+        dp[0] = 1; // empty t can always be formed (one way: pick nothing)
+
+        for (int i = 1; i <= m; i++) {
+            // iterate j backwards so dp[j-1] still refers to previous row's value
+            for (int j = n; j >= 1; j--) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return (int)dp[n];
+    }
+};
