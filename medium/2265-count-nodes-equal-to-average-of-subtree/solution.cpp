c903c6b7ec7b89d4757c3240ee220e8268f533bd
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-10 22:20:23 +0530

    ✅ Count Nodes Equal to Average of Subtree | Medium | C++ | Runtime: 11 ms (27.56%) | Memory: 15.8 MB | 2026-09-10

--- medium/2265-count-nodes-equal-to-average-of-subtree/solution.cpp
+++ medium/2265-count-nodes-equal-to-average-of-subtree/solution.cpp
@@ -1,18 +1,44 @@
-        // Calculate current subtree
-        int sum = left.first + right.first + root->val;
-        int count = left.second + right.second + 1;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
 
-        // Check average
-        if (root->val == sum / count) {
-            ans++;
-        }
+    // returns {sum, count}
+    pair<int, int> dfs(TreeNode* root) {
+        if (root == NULL) {
+            return {0, 0};
+        }
 
+        // Get information from left subtree
+        pair<int, int> left = dfs(root->left);
 
-        pair<int, int> right = dfs(root->right);
-        // Get information from right subtree
+        // Get information from right subtree
+        pair<int, int> right = dfs(root->right);
 
-        pair<int, int> left = dfs(root->left);
-        // Get information from left subtree
+        // Calculate current subtree
+        int sum = left.first + right.first + root->val;
+        int count = left.second + right.second + 1;
 
-        }
-            return {0, 0};
+        // Check average
+        if (root->val == sum / count) {
+            ans++;
+        }
+
+        return {sum, count};
+    }
+
+    int averageOfSubtree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
