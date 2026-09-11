923e7ce483500d1114cfb68177ef309cede37774
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-11 21:07:10 +0530

    ✅ Unique 3-Digit Even Numbers | Easy | C++ | Runtime: 0 ms | Memory: 8.1 MB | 2026-09-11

---
+++ easy/3483-unique-3-digit-even-numbers/solution.cpp
@@ -0,0 +1,15 @@
+
+            for (int j = 0; j < digits.size(); j++) {
+                continue;
+
+            if (digits[i] == 0)
+            // First digit cannot be 0
+
+        for (int i = 0; i < digits.size(); i++) {
+
+        int ans = 0;
+
+        bool seen[1000] = {};
+    int totalNumbers(vector<int>& digits) {
+public:
+class Solution {
