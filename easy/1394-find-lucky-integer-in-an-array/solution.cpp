0bbacdc53afca8b0ec7874ed1177277e1dbcb5cf
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-11 21:16:32 +0530

    ✅ Find Lucky Integer in an Array | Easy | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-11

--- easy/1394-find-lucky-integer-in-an-array/solution.cpp
+++ easy/1394-find-lucky-integer-in-an-array/solution.cpp
@@ -1,17 +1,21 @@
-            hash[arr[i]]++;
-        }
-        for(int i =1;i<=n;i++)
-        {
-            if(i==hash[i])
-            {
-               luckynum = hash[i];
-        {
-        for(int i =0;i<n;i++)
-        vector<int>hash(n+1,0);
-            
-            }
-        }
-        return luckynum;
-        int luckynum=-1;
-        int n = arr.size();
-    int findLucky(vector<int>& arr) {
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int n = arr.size();
+        int luckynum=-1;
+        vector<int>hash(n+1,0);
+        for(int i =0;i<n;i++)
+        {
+            hash[arr[i]]++;
+        }
+        for(int i =1;i<=n;i++)
+        {
+            if(i==hash[i])
+            {
+               luckynum = hash[i];
+            
+            }
+        }
+        return luckynum;
+    }
+};
