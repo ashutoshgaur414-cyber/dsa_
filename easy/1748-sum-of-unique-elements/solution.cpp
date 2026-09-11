547f6ba2a5e30c4f6095efd238c63f2a59926f5f
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-11 21:22:55 +0530

    ✅ Sum of Unique Elements | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 10.7 MB | 2026-09-11

--- easy/1748-sum-of-unique-elements/solution.cpp
+++ easy/1748-sum-of-unique-elements/solution.cpp
@@ -1,14 +1,19 @@
-        for(int i =0;i<nums.size();i++)
-        {
-            hash[nums[i]]++;
-        }
-        for(int i =0;i<=100;i++)
-        {
-            if(hash[i]==1)
-            {
-              sum = sum +i;
-            }
-        }
-        return sum;
-    }
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        vector<int>hash(101,0);
+        int sum = 0;
+        for(int i =0;i<nums.size();i++)
+        {
+            hash[nums[i]]++;
+        }
+        for(int i =0;i<=100;i++)
+        {
+            if(hash[i]==1)
+            {
+              sum = sum +i;
+            }
+        }
+        return sum;
+    }
 };
