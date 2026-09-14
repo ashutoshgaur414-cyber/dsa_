ff5feb4971310fc4b5026aea8f458c2a5272af88
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-14 19:49:35 +0530

    ✅ Rectangle Overlap | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 9.8 MB | 2026-09-14

--- easy/0836-rectangle-overlap/solution.cpp
+++ easy/0836-rectangle-overlap/solution.cpp
@@ -1,18 +1,16 @@
-      rec2[0]);
-      int overlapHeight = min(rec1[3], rec2[3]) - max(rec1[1], 
-      rec2[1]);
-
-      if(overlapWidth >0 && overlapHeight >0 )
-      {
-        return true;
-      }
-      else
-      {
-        return false;
-      }
-    }
-      int overlapWidth  = min(rec1[2], rec2[2]) - max(rec1[0], 
-    {
-    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) 
+class Solution {
 public:
-class Solution {
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+      int overlapWidth  = min(rec1[2], rec2[2]) - max(rec1[0], rec2[0]);
+      int overlapHeight = min(rec1[3], rec2[3]) - max(rec1[1], rec2[1]);
+
+      if(overlapWidth >0 && overlapHeight >0 )
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+};
