40cbf62fd03750e5865fab2769a9110f3a9d4661
ashutoshgaur414-cyber <ashutoshgaur414@gmail.com>
2026-09-22 21:49:20 +0530

    ✅ Find X Value of Array II | Hard | C++ | Runtime: 345 ms (43.64%) | Memory: 298.7 MB | 2026-09-22

---
+++ hard/3525-find-x-value-of-array-ii/solution.cpp
@@ -0,0 +1,141 @@
+class Solution {
+public:
+
+    struct Node {
+        int prod;
+        array<int, 5> cnt{};
+
+        Node() {
+            prod = 1;
+            cnt.fill(0);
+        }
+    };
+
+    int k;
+    vector<Node> tree;
+
+    Node merge(const Node& left, const Node& right) {
+
+        Node res;
+
+        res.prod = (left.prod * right.prod) % k;
+
+        // Prefixes completely inside left
+        for (int r = 0; r < k; r++) {
+            res.cnt[r] += left.cnt[r];
+        }
+
+        // Prefixes that continue from left into right
+        for (int r = 0; r < k; r++) {
+            int rem = (left.prod * r) % k;
+            res.cnt[rem] += right.cnt[r];
+        }
+
+        return res;
+    }
+
+    void build(vector<int>& nums, int node, int l, int r) {
+
+        if (l == r) {
+
+            int rem = nums[l] % k;
+
+            tree[node].prod = rem;
+            tree[node].cnt[rem] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(nums, node * 2, l, mid);
+        build(nums, node * 2 + 1, mid + 1, r);
+
+        tree[node] = merge(tree[node * 2],
+                           tree[node * 2 + 1]);
+    }
+
+    void update(int node, int l, int r,
+                int idx, int val) {
+
+        if (l == r) {
+
+            tree[node] = Node();
+
+            int rem = val % k;
+
+            tree[node].prod = rem;
+            tree[node].cnt[rem] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (idx <= mid)
+            update(node * 2, l, mid, idx, val);
+        else
+            update(node * 2 + 1, mid + 1, r, idx, val);
+
+        tree[node] = merge(tree[node * 2],
+                           tree[node * 2 + 1]);
+    }
+
+    Node query(int node, int l, int r,
+               int ql, int qr) {
+
+        if (ql <= l && r <= qr)
+            return tree[node];
+
+        int mid = (l + r) / 2;
+
+        if (qr <= mid)
+            return query(node * 2, l, mid, ql, qr);
+
+        if (ql > mid)
+            return query(node * 2 + 1, mid + 1, r, ql, qr);
+
+        Node left =
+            query(node * 2, l, mid, ql, qr);
+
+        Node right =
+            query(node * 2 + 1, mid + 1, r, ql, qr);
+
+        return merge(left, right);
+    }
+
+    vector<int> resultArray(vector<int>& nums,
+                            int K,
+                            vector<vector<int>>& queries) {
+
+        k = K;
+
+        int n = nums.size();
+
+        tree.resize(4 * n + 5);
+
+        build(nums, 1, 0, n - 1);
+
+        vector<int> ans;
+        ans.reserve(queries.size());
+
+        for (auto &q : queries) {
+
+            int index = q[0];
+            int value = q[1];
+            int start = q[2];
+            int x = q[3];
+
+            // Persistent update
+            update(1, 0, n - 1, index, value);
+
+            // Query [start, n-1]
+            Node res =
+                query(1, 0, n - 1, start, n - 1);
+
+            ans.push_back(res.cnt[x]);
+        }
+
+        return ans;
+    }
+};
